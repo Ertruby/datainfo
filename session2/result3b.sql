c0e209f3d7cf4e8f2b8b4b65b01f04bec7360d05
Mark van Doesum <mvandoesum@hotmail.com>
2014-05-06 16:38:17 +0200

    Shunting rule 1x gebruikt

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,11 +1,8 @@
-﻿-- Als eerst halen we de laatste subquery weg.
-
-SELECT p.pid
-FROM Person p
-WHERE EXISTS (
+﻿SELECT DISTINCT p.pid FROM Person p, Acts a
+WHERE a.pid = p.pid
+AND EXISTS (
 	SELECT *
-	FROM Acts a, Movie m
-	WHERE a.pid = p.pid
-	AND m.mid = a.mid
+	FROM Movie m
+	WHERE m.mid = a.mid
 	AND m.name = 'Back to the Future'
-)
+);
