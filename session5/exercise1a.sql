a3d37363324be45cf599a765a45a4adc72ab56a5
Mark van Doesum <mvandoesum@hotmail.com>
2014-05-27 14:02:09 +0200

    1a

---
+++ session5/exercise1a.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Boek (
+	isbn int, 
+	titel varchar(255),
+	auteur varchar(255),
+	PRIMARY KEY (isbn)
+	);
+
+CREATE TABLE Exemplaar (
+	isbn int,
+	volgnummer int,
+	gewicht int,
+	kast varchar(5),
+	FOREIGN KEY (isbn) REFERENCES Boek(isbn) ON DELETE CASCADE ON UPDATE CASCADE
+	PRIMARY KEY (isbn, volgnummer)
+	);
